c251bc327668804995dd3a867971f57254e761be
Kittipat Phongsak <56060256+Pkittipat@users.noreply.github.com>
2020-02-25 16:10:56 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Pkittipat/combineone.git",
+    url="https://github.com/Pkittipat/combineone/tree/0.0.2",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
