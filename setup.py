f8197bd43606dd596f11b24cd56b74f452b4d9a5
Pkittipat <kittipat.phongsak@gmail.com>
2020-02-25 16:09:20 +0700

    testt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="combineeone",
-    version="0.0.1",
+    version="0.0.2",
     author="pkittipat",
     author_email="kittipat.phongsak@gmail.com",
     description="A small example package",
